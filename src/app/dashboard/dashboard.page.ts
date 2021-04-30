406c45233982159606b64412a54b55f99100440c
lyf.email@qq.com <lyf.email@qq.com>
2021-04-30 16:41:11 +0800

    dark mode and add dashboard data

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Config, IonList } from '@ionic/angular';
+import { ConferenceData } from '../providers/conference-data';
+import { UserData } from '../providers/user-data';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  // Gets a reference to the list element
+  @ViewChild('scheduleList', { static: true }) scheduleList: IonList;
+
+  ios: boolean;
+  dayIndex = 0;
+  queryText = '';
+  segment = 'all';
+  excludeTracks: any = [];
+  shownSessions: any = [];
+  groups: any = [];
+  confDate: string;
+  showSearchbar: boolean;
+
+  constructor(
+    public confData: ConferenceData,
+    public user: UserData,
+    public config: Config
+  ) { }
 
   ngOnInit() {
+    this.updateSchedule();
+  }
+
+  updateSchedule() {
+    // Close any open sliding items when the schedule updates
+    if (this.scheduleList) {
+      this.scheduleList.closeSlidingItems();
+    }
+
+    this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
+      this.shownSessions = data.shownSessions;
+      this.groups = data.groups;
+    });
   }
 
 }
